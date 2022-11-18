b9a1e1cb16299b3ec42442598e16d946c836446f
John Sinalsal <sjohnsinalsal@gmail.com>
2022-11-18 19:54:29 +0700

    First Submission by <John Sinalsal>

---
+++ OneDrive - Institut Teknologi Bandung/Pengkom/git projects/Hands-On-Git-Intro-master/John Sinalsal_ITB/identitas.md
@@ -0,0 +1,4 @@
+# First submission by John Sinalsal
+
+## Screenshot
+![Screenshot](Screenshot 2022-11-18 194407.png)
---
+++ OneDrive - Institut Teknologi Bandung/Pengkom/git projects/Hands-On-Git-Intro-master/John Sinalsal_ITB/main.py
@@ -0,0 +1,4 @@
+print("Nama: John Sinalsal Saragih")
+print("Jurusan: TPB-STEI")
+print("Universitas: ITB")
+print("Alasan ingin masuk GDSC: Ingin meluaskan wawasan terhadap dunia programming")

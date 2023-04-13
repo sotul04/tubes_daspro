a5ae97853a27dd149786f3bd74b0aded87c3ebc0
sotul04 <109208105+sotul04@users.noreply.github.com>
2023-04-13 18:24:29 +0700

    update fungsi utama 4/13/2023

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-for i in range (10,11):
-    print(i)
+from spesification import *
+from data_manipulate import *
+from function import *
+from data_type import *
+
+folder = [0]
+#[F13] melakukan load {memanggil file eksternal}
+load(folder)
+folder = folder[0]
+u_path = folder+"/user.csv"
+c_path = folder+"/candi.csv"
+b_path = folder+"/bahan_bangunan.csv"
+user_data = user(read_user(u_path)[0],read_user(u_path)[1])
+candi_data = candi(read_candi(c_path)[0],read_candi(c_path)[1])
+bahan_data = bahan(read_bahan(b_path)[0],read_bahan(b_path)[1])
+print(user_data.detail[:user_data.Neff])
+print(candi_data.detail[:candi_data.Neff])
+print(bahan_data.detail[:bahan_data.Neff])
+
+while True:
+    islogin = False
+    command = input()
+    pass

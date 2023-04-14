c2278b824016a451fcd3631c929280a8a1aa0bf7
sotul04 <109208105+sotul04@users.noreply.github.com>
2023-04-14 21:26:37 +0700

    Penambahan class number_colc untuk fungsi random

--- data_type.py
+++ data_type.py
@@ -1,5 +1,6 @@
-"""Memuat classs untuk tipe bentukan yang dimuat dari file eksternal"""
+"""Memuat classs untuk tipe bentukan"""
 
+# tipe bentukan yang menyimpan data dari FILE EKSTERNAL
 class user:
 # tipe bentukan
 # type user : <Neff : integer {jumlah efektif};
@@ -29,3 +30,12 @@ class bahan:
         self.Neff = Neff
         self.detail = detail
         self.length = 3
+
+# tipe bentukan yang menyimpan data log angka hasil RANDOMISASI
+class number_colc:
+# tipe bantukan
+# type number_colc : <Neff : integer {jumlah efektif};
+#                     array : array of integer {menyimpan log data random} >
+    def __init__(self,Neff,array):
+        self.Neff = Neff
+        self.array = array

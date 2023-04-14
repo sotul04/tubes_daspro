d2278578dd85632981a349805e46875035f5572e
sotul04 <109208105+sotul04@users.noreply.github.com>
2023-04-14 21:29:01 +0700

    Perubahan - Suthasoma

--- main.py
+++ main.py
@@ -4,15 +4,22 @@ from function import *
 from data_type import *
 
 folder = [0]
-#[F13] melakukan load {memanggil file eksternal}
+# [F13] melakukan load {memanggil file eksternal}
 load(folder)
 folder = folder[0]
 u_path = folder+"/user.csv"
 c_path = folder+"/candi.csv"
 b_path = folder+"/bahan_bangunan.csv"
+# memuat semua data dari file eksternal dan disimpan untuk dilakukan permainan game
 user_data = user(read_user(u_path)[0],read_user(u_path)[1])
 candi_data = candi(read_candi(c_path)[0],read_candi(c_path)[1])
 bahan_data = bahan(read_bahan(b_path)[0],read_bahan(b_path)[1])
+# memuat numbers {variabel yang menyimpan angka acak selama permainan}
+if candi_data.Neff > 1:
+    numbers = number_colc(1, candi_data.detail[candi_data.Neff-1][4])
+else:
+    numbers = number_colc(1, user_data.Neff)
+
 #print(user_data.detail[:user_data.Neff])
 #print(candi_data.detail[:candi_data.Neff])
 #print(bahan_data.detail[:bahan_data.Neff])
--- test.py
+++ test.py
@@ -1,17 +1,10 @@
-from data_manipulate import *
 from data_type import *
 from function import *
 
-folder = "data_eksternal\\"
-u = folder+"user.csv"
-c = folder+"candi.csv"
-b = folder+"bahan_bangunan.csv"
+numbers = number_colc(1, [1])
 
-data_candi = candi(read_candi(c)[0], read_candi(c)[1])
-#remove_candi("chiko", data_candi)
-#add_candi("filbert", [3,1,5], data_candi)
-#add_candi("mattheuw", [1,4,5], data_candi)
-#add_candi("chiko", [5,5,5], data_candi)
-
-for i in range (1,data_candi.Neff):
-    print(data_candi.detail[i])
+for i in range(100):
+    print(acak_kumpul(numbers))
+print("="*50)
+for i in range(100):
+    print(acak_bangun(numbers))

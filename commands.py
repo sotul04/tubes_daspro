7510de9289fbe02b6e84a191531b941a28565f39
sotul04 <109208105+sotul04@users.noreply.github.com>
2023-04-15 10:40:24 +0700

    Penambahan spesifikasi F08 - Suthasoma

--- commands.py
+++ commands.py
@@ -47,4 +47,61 @@ def save(user,candi,bahan):
             print(f"\nBerhasil menyimpan data di folder {fold_temp}!\n")
 
 
-    
+#F08
+def batch_kumpul(user,bahan,numbers):
+# melakukan fitur F08 - batchkumpul
+    data = list_jin("jin_pengumpul", user)
+    racikan = [0,0,0]
+    if data[0] == 0:
+        print("Kumpul gagal. Anda tidak punya jin pengumpul. Silahkan summon terlebih dahulu.")
+    else:
+        print(f"Mengerahkan {data[0]} jin untuk mengumpulkan bahan.")
+        for i in range (data[0]):
+            pasir = acak_kumpul(numbers)
+            batu = acak_kumpul(numbers)
+            air = acak_kumpul(numbers)
+            racikan[0] += pasir
+            racikan[1] += batu
+            racikan[2] += air
+        print(f"Jin menemukan total {racikan[0]} pasir, {racikan[1]} batu, {racikan[2]} air.")
+        update_bahan(racikan, bahan)
+
+def batch_bangun(user,candi,bahan,numbers):
+# melakukan fitur F08 - batchbangun
+    data = list_jin("jin_pembangun", user)
+    bahan_total = [0,0,0]
+    if data[0] == 0:
+        print("Bangun gagal. Anda tidak punya jin pembangun. Silahkan summon terlebih dahulu.")
+    else:
+        temp = [[] for i in range(data[0])]
+        for i in range(data[0]):
+            pasir = acak_bangun(numbers)
+            batu = acak_bangun(numbers)
+            air  = acak_bangun(numbers)
+            temp[i] = [data[1][i],pasir,batu,air]
+            bahan_total[0] += pasir
+            bahan_total[1] += batu
+            bahan_total[2] += air
+        bahan_cukup = True
+        for i in range(3):
+            if bahan.detail[i+1][2] < bahan_total[i]:
+                bahan_cukup = False
+                break
+        print(f"Mengerahkan {data[0]} jin untuk membangun candi dengan total bahan {bahan_total[0]} pasir, {bahan_total[1]} batu, dan {bahan_total[2]} air.")
+        if bahan_cukup:
+            update_bahan([-bahan_total[0],-bahan_total[1],-bahan_total[2]], bahan)
+            for i in range(data[0]):
+                add_candi(temp[i][0], [temp[i][1],temp[i][2],temp[i][3]], candi)
+            print(f"Jin berhasil membangun total {data[0]} candi.")
+        else:
+            kurang = [0,0,0]
+            for i in range (3):
+                kurang[i] = bahan_total[i]-bahan.detail[i+1][2]
+                if kurang[i] < 0:
+                    kurang[i] = 0
+            print(f"Bangun gagal. Kurang {kurang[0]} pasir, {kurang[1]} batu, {kurang[2]} air.")
+            
+
+
+
+

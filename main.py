450cb92895509ed6201ee7ba8863ebcd4686fb31
sotul04 <109208105+sotul04@users.noreply.github.com>
2023-04-16 00:56:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from spesification import *
+from commands import *
 from data_manipulate import *
 from function import *
 from data_type import *
@@ -16,16 +16,26 @@ candi_data = candi(read_candi(c_path)[0],read_candi(c_path)[1])
 bahan_data = bahan(read_bahan(b_path)[0],read_bahan(b_path)[1])
 # memuat numbers {variabel yang menyimpan angka random selama permainan}
 if candi_data.Neff > 1:
-    numbers = number_colc(1, candi_data.detail[candi_data.Neff-1][4])
+    numbers = number_colc(1, [candi_data.detail[candi_data.Neff-1][4]])
 else:
-    numbers = number_colc(1, user_data.Neff)
+    numbers = number_colc(1, [user_data.Neff])
 
-#print(user_data.detail[:user_data.Neff])
-#print(candi_data.detail[:candi_data.Neff])
-#print(bahan_data.detail[:bahan_data.Neff])
-
-#save(user_data, candi_data, bahan_data)
-#while True:
-#    islogin = False
-#    command = input()
-#    pass
+role = None
+while True:
+    command = input(">>> ")
+    if command == "login":
+        login(user_data, candi_data, bahan_data, numbers, role)
+    elif command == "save":
+        save(user_data, candi_data, bahan_data)
+    elif command == "exit":
+        simpan = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ")
+        while simpan != "y" and simpan != "Y" and simpan != "n" and simpan != "N":
+            simpan = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ")
+        if simpan == "y" or simpan == "Y":
+            save(user_data, candi_data, bahan_data)
+        exit()
+    elif command == "logout":
+        print("Logout gagal!")
+        print("Anda belum login, silahkan login terlebih dahulu sebelum melakukan logout.\n")
+    else:
+        print("Anda tidak bisa melakukan command tersebut karena Anda belum login atau akun Anda tidak memiliki akses.")

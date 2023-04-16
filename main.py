d192c84162bba69c29da9001cbbb3e17d84eebc5
sotul04 <109208105+sotul04@users.noreply.github.com>
2023-04-16 19:57:49 +0700

    Revisi ~ untuk random

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from commands import *
 from data_manipulate import *
 from function import *
 from data_type import *
+from time import time
 
 folder = ["kosong"]
 # [F13] melakukan load {memanggil file eksternal}
@@ -15,11 +16,9 @@ user_data = user(read_user(u_path)[0],read_user(u_path)[1])
 candi_data = candi(read_candi(c_path)[0],read_candi(c_path)[1])
 bahan_data = bahan(read_bahan(b_path)[0],read_bahan(b_path)[1])
 # memuat numbers {variabel yang menyimpan angka random selama permainan}
-if candi_data.Neff > 1:
-    numbers = number_colc(1, [candi_data.detail[candi_data.Neff-1][4]])
-else:
-    numbers = number_colc(1, [user_data.Neff])
+numbers = number_colc(1, [int(time)%11103515245])
 
+# PERMAINAN
 role = None
 while True:
     command = input(">>> ")
